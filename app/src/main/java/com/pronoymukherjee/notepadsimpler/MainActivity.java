315c1d7226cf5b2acbdbf73f484d74d96793ef2f
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2016-11-30 21:31:39 +0530

    Fixed the issue to keep the new line character.

--- app/src/main/java/com/pronoymukherjee/notepadsimpler/MainActivity.java
+++ app/src/main/java/com/pronoymukherjee/notepadsimpler/MainActivity.java
@@ -11,55 +11,85 @@ import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
-    String fileName="Note.txt";
+    String fileName = "Note.txt";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText=(EditText) findViewById(R.id.text);
+        editText = (EditText) findViewById(R.id.text);
     }
-    protected void setData(){
-        try{
+
+    protected void setData() {
+        try {
             FileInputStream fileInputStream = openFileInput(fileName);
-            Scanner obj=new Scanner(fileInputStream);
-            String t="";
-            while(obj.hasNext())
-                t+=obj.nextLine();
+            Scanner obj = new Scanner(fileInputStream);
+            String t = "";
+            while (obj.hasNext())
+                t += obj.nextLine();
+            t = setFormat(t);
             editText.setText(t);
             fileInputStream.close();
             obj.close();
-        }
-        catch (Exception e){
-            Toast.makeText(this,"No previous file found.",Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "No previous file found.", Toast.LENGTH_LONG).show();
         }
     }
-    protected void saveData(){
-        String text=editText.getText().toString();
+
+    protected void saveData() {
+        String text = editText.getText().toString();
         try {
             FileOutputStream fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
-            text=text.trim();
+            text = text.trim();
+            text = editData(text);
             fileOutputStream.write(text.getBytes());
-        }
-        catch (Exception e){
-            Toast.makeText(this,"ERROR saving file.",Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "ERROR saving file.", Toast.LENGTH_LONG).show();
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(!editText.getText().toString().equals("")) {
+        if (!editText.getText().toString().equals("")) {
             saveData();
-            Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show();
-        }
-        else{
+            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show();
+        } else {
             saveData();
-            Toast.makeText(this,"Nothing to Save.",Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Nothing to Save.", Toast.LENGTH_LONG).show();
         }
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         setData();
     }
+
+    public String editData(String text) {
+        int l = text.length();
+        char ch;
+        String temp = "";
+        for (int i = 0; i < l ; i++) {
+            ch = text.charAt(i);
+            if (ch == '\n') {
+                temp += " ''n ";
+            } else temp += ch;
+        }
+        return temp;
+    }
+    public String setFormat(String text){
+        int l=text.length();
+        int i;char ch;String temp="";
+        for(i=0;i<l;i++){
+            ch=text.charAt(i);
+            if(ch=='\'' && text.charAt(i+1)=='\'' && text.charAt(i+2)=='n'){
+                temp+="\n";
+                i+=2;
+            }
+            else temp+=ch;
+        }
+        return temp;
+    }
 }
+
